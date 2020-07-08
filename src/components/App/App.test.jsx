f432ca69b6d59ee7824f51e0c7818dd34c4add35
drmeloy <drmeloy@gmail.com>
2020-07-08 16:35:44 -0700

    component snapshot tests written

--- src/components/App/App.test.jsx
+++ src/components/App/App.test.jsx
@@ -3,9 +3,8 @@ import { shallow } from 'enzyme';
 import App from './App';
 
 describe('App component', () => {
-  it('renders App', () => {
+  it('matches snapshot', () => {
     const wrapper = shallow(<App />);
     expect(wrapper).toMatchSnapshot();
   });
 });
-  
