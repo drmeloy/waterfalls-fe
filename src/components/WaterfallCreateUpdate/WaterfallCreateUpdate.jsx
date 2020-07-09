6219b928d219a663ea691b366a55e9a33948a6f6
drmeloy <drmeloy@gmail.com>
2020-07-09 12:50:45 -0700

    form styled and updated to use react-bootstrap

--- src/components/WaterfallCreateUpdate/WaterfallCreateUpdate.jsx
+++ src/components/WaterfallCreateUpdate/WaterfallCreateUpdate.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { createWaterfall, updateWaterfall, getWaterfall } from '../../services/waterfallsApi';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 export default function WaterfallCreateUpdate({ match: { params } }){
   const [pk, setPk] = useState('');
@@ -55,33 +57,44 @@ export default function WaterfallCreateUpdate({ match: { params } }){
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div className="form-group">
-        <label>Name (required):</label>
-        <input className="form-control" type="text" value={name} onChange={({ target }) => setName(target.value)} />
-
-        <label>Height (required):</label>
-        <input className="form-control" type="text" value={height} onChange={(({ target }) => setHeight(target.value))} />
-
-        <label>Latitude (required):</label>
-        <input className="form-control" type="text" value={latitude} onChange={(({ target }) => setLatitude(target.value))} />
-        
-        <label>Longitude (required):</label>
-        <input className="form-control" type="text" value={longitude} onChange={(({ target }) => setLongitude(target.value))} />
-
-        <label>Description (optional):</label>
-        <textarea className="form-control" value={description} onChange={(({ target }) => setDescription(target.value))} />
-
-        <input className="btn btn-primary" type="submit" value="Submit" />
-      </div>
-    </form>
+    <Form onSubmit={handleSubmit} className='mt-5 p-3'>
+      <Form.Group controlId='name'>
+        <Form.Label>Name (required):</Form.Label>
+        <Form.Control type='text' value={name} onChange={({ target }) => setName(target.value)} />
+      </Form.Group>
+      <Form.Group controlId='height'>
+        <Form.Label>Height (required):</Form.Label>
+        <Form.Control type='text' value={height} onChange={({ target }) => setHeight(target.value)} />
+      </Form.Group>
+      <Form.Group controlId='latitude'>
+        <Form.Label>Latitude (required):</Form.Label>
+        <Form.Control type='text' value={latitude} onChange={(({ target }) => setLatitude(target.value))} />
+        <Form.Text className='text-muted'>
+          Please include only the numbers and negative and period symbols, not the cardinal direction indicators. For example, 45.2987 S is written -45.2987.
+        </Form.Text>
+      </Form.Group>
+      <Form.Group controlId='longitude'>
+        <Form.Label>Longitude (required):</Form.Label>
+        <Form.Control type='text' value={longitude} onChange={(({ target }) => setLongitude(target.value))} />
+        <Form.Text className='text-muted'>
+          Please include only the numbers and negative and period symbols, not the cardinal direction indicators. For example, 122.8624 W is written -122.8624.
+        </Form.Text>
+      </Form.Group>
+      <Form.Group controlId='description'>
+        <Form.Label>Description (optional):</Form.Label>
+        <Form.Control type='text' as='textarea' value={description} onChange={(({ target }) => setDescription(target.value))} />
+      </Form.Group>
+      <Button variant='primary' type='submit'>
+        Submit
+      </Button>
+    </Form>
   ); 
 }
 
 WaterfallCreateUpdate.propTypes = {
   match: PropTypes.shape({
     params: PropTypes.shape({
-      id: PropTypes.number.isRequired
+      id: PropTypes.number
     })
   })
 };
