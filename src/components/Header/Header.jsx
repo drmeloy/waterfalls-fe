93abe3c1abe8e3eae85248fb7e5d7bc4a8e0aa31
drmeloy <drmeloy@gmail.com>
2020-07-10 15:21:09 -0700

    header links

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,12 +7,8 @@ export default function Header(){
   return (
     <NavBar bg='primary' expand='sm' variant='dark' fixed='top' className='justify-content-between align-middle border border-dark'>
       <NavBar.Brand href="/" style={{ 'fontSize': '1.5em' }}>Waterfalls of Oregon!</NavBar.Brand>
-      <Nav>
-        <Nav.Item>
-          <Link to='/waterfall/'>
-            <Nav.Link style={{ 'transition': '.3s' }}>Create new waterfall</Nav.Link>
-          </Link>
-        </Nav.Item>
+      <Nav style={{ 'transition': '.3s' }}>
+        <Link to='/waterfall/'>Create new waterfall</Link>
       </Nav>       
     </NavBar>
   );
