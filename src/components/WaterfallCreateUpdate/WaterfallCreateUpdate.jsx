067494991125ac5f503334f8fd57ba2bea22fbc3
drmeloy <drmeloy@gmail.com>
2020-07-09 18:29:33 -0700

    changed API URL in waterfallsApi.js

--- src/components/WaterfallCreateUpdate/WaterfallCreateUpdate.jsx
+++ src/components/WaterfallCreateUpdate/WaterfallCreateUpdate.jsx
@@ -115,7 +115,7 @@ export default function WaterfallCreateUpdate({ match: { params } }){
 WaterfallCreateUpdate.propTypes = {
   match: PropTypes.shape({
     params: PropTypes.shape({
-      id: PropTypes.number
+      id: PropTypes.string
     })
   })
 };
