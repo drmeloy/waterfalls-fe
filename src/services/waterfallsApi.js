961db808cd1ca950cb94401a2bf1c3adc80dc4ab
drmeloy <drmeloy@gmail.com>
2020-07-13 15:15:48 -0700

    routing

--- src/services/waterfallsApi.js
+++ src/services/waterfallsApi.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 const API_URL = '//ec2-18-223-111-255.us-east-2.compute.amazonaws.com:8000/api/waterfalls/';
 
-export const getWaterfalls = () => axios.get(API_URL).then(res => res.data.sort((a, b) => a.id - b.id));
+export const getWaterfalls = () => axios.get(`${API_URL}`).then(res => res.data.sort((a, b) => a.id - b.id));
 
 export const getWaterfall = id => axios.get(`${API_URL}${id}/`).then(res => res.data);
 
-export const createWaterfall = waterfall => axios.post(API_URL, waterfall);
+export const createWaterfall = waterfall => axios.post(`${API_URL}`, waterfall);
 
 export const updateWaterfall = waterfall => axios.put(`${API_URL}${waterfall.id}/`, waterfall);
 
